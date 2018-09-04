e55888afa8d606beb0f894602c717fa6f40361c4
Bethea Davida <betheazia17@gmail.com>
2018-09-04 21:17:39 +0700

    editing file

--- 2/normalize.py
+++ 2/normalize.py
@@ -3,15 +3,6 @@ from scipy.misc import imsave
 import sys
 
 def normalize(img):
-  '''
-  Normalize the exposure of an image.
-  @args:
-    {numpy.ndarray} img: an array of image pixels with shape:
-      (height, width)
-  @returns:
-    {numpy.ndarray} an image with shape of `img` wherein
-      all values are normalized such that the min=0 and max=255
-  '''
   _min = img.min()
   _max = img.max()
   return img - _min * 255 / (_max - _min)
