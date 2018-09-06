9b136aaf7ed1c78210a7b2a4bf6bb33433c2cbc6
X550ZE\David <davidtheoxcalibur@gmail.com>
2018-09-06 18:12:00 +0700

    add tugas 2

--- 2/app.py
+++ 2/app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, render_template, request, redirect
 from rgbhist import *
+from normalize import *
 import matplotlib
 matplotlib.use('Agg')
 
 from matplotlib import pyplot as plt
+from PIL import Image
 
 from werkzeug import secure_filename
 import time
@@ -14,14 +16,17 @@ UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.route("/")
+def main():
+    return render_template('index.html')
+
 @app.route("/1")
-def main():
-    return render_template(
-        '1.html')
+def main1():
+    return render_template('1.html')
+
 @app.route("/2")
-def main():
-    return render_template(
-        '2.html')
+def main2():
+    return render_template('2.html')
 
 @app.route("/histogram", methods=['POST'])
 def show_histogram():
@@ -58,11 +63,39 @@ def show_histogram():
             plotcolor = (0.5, 0.5, 0.5)
         plt.plot(build_hist(UPLOAD_FOLDER + '/' + secure_filename(image.filename), color), color=plotcolor)
 
-
     plt.savefig('static/images/plot.png')
-    return  render_template('plot.html', title = colorname + ' histogram', url ='static/images/plot.png?' + str(time.time()))
+    return render_template('plot.html', title = colorname + ' histogram', url ='static/images/plot.png?' + str(time.time()))
 
-    @app.route("/equalizer", methods=['POST'])
+@app.route("/normalize", methods=['POST'])
+def show_normalized():
+    if 'imgFile' not in request.files:
+        return 'No selected file'
+    file = request.files['imgFile']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return 'No selected file'
+    image = request.files['imgFile']
+    method = request.form.get('method')
+    img_path = 'static/images/image.png'
+    image.save(img_path)
+    if method == 'k':
+        # kumulatif
+        title = 'Cumulative'
+        imagee = Image.open(img_path)
+        new_image = normalize(imagee)
+        norm_img_path = 'static/images/normalized_image.png'
+        new_image.save(norm_img_path)
+    else:
+        # scaling
+        title = 'Scaling'
+        base_image = Image.open(img_path)
+        width, height = base_image.size
+        normalized_img = scaling(base_image, width, height)
+        norm_img_path = 'static/images/normalized_image.png'
+        normalized_img.save(norm_img_path)
+        
+    return render_template('result.html', title = 'Normalized Picture (' + title + ')', url_before = img_path + '?' + str(time.time()), url_after = norm_img_path + '?' + str(time.time()))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=os.environ['PORT'])
--- 2/normalize.py
+++ 2/normalize.py
@@ -1,12 +1,77 @@
-from scipy.ndimage import imread
-from scipy.misc import imsave
 import sys
+import os.path
+import math
+import numpy as np
+from PIL import Image
 
-def normalize(img):
-  _min = img.min()
-  _max = img.max()
-  return img - _min * 255 / (_max - _min)
+def calculate_image_matrix_histogram(matrix):
+    _, _, channel = matrix.shape
+    flat_matrix = matrix.flatten()
+    histogram = np.zeros((256, channel))
+    for i, val in enumerate(flat_matrix):
+        histogram[val][i % channel] += 1
+    return histogram
 
-img = imread(sys.argv[1])
-normalized = normalize(img)
-imsave('normalized.png', normalized)
+def equalize_image_matrix(matrix):
+    map_value_to_cumulative = {}
+
+    height, width, channel = matrix.shape
+    cumulative_value = np.zeros(channel)
+    histogram = calculate_image_matrix_histogram(matrix)
+    for value, freq in enumerate(histogram):
+        cumulative_value = [cumulative_value[i] + freq[i] for i in range(channel)]
+        map_value_to_cumulative[value] = cumulative_value
+
+    total_frequency = width * height
+    result = np.zeros(matrix.shape)
+    for y in range(height):
+        for x in range(width):
+            initial_value = matrix[y][x]
+            result[y][x] = [math.floor(float(map_value_to_cumulative[v][c]) / float(total_frequency) * 255) for c, v in enumerate(initial_value)]
+    
+    return result
+
+def normalize(base_image):
+    MAX_WIDTH = 600
+    MAX_HEIGHT = 400
+    try:
+        width, height = base_image.size
+        print(width, height)
+        if width > MAX_WIDTH:
+            height, width = height * MAX_WIDTH / width, MAX_WIDTH
+        if height > MAX_HEIGHT:
+            width, height = width * MAX_HEIGHT / height, MAX_HEIGHT
+        base_image = base_image.resize((int(width), int(height)))
+    except Exception as e:
+        print("Something wrong", e)
+        sys.exit(1)
+
+    result_matrix = equalize_image_matrix(np.array(base_image))
+    result_image = Image.fromarray(np.uint8(result_matrix))
+    return result_image
+
+def scaling(base_image,w,h):
+    histogram = calculate_image_matrix_histogram(np.array(base_image))
+    # area = (width * height * 9)/10
+    sum_hist = 0
+    for i in range(len(histogram)):
+        sum_hist += (histogram[i][0] + histogram[i][1] + histogram[i][2])/3
+    covered_area = sum_hist * 0.9
+    sum_hist = 0
+    for i in range(len(histogram)):
+        sum_hist += (histogram[i][0] + histogram[i][1] + histogram[i][2])/3
+        if(sum_hist > covered_area):
+            _max = i
+            break
+    _min = 0
+    normalized_img = np.array(base_image) * (255 / _max)
+    for i in normalized_img:
+        for j in i:
+            if(j[0]>255):
+                j[0] = 255
+            if(j[1]>255):
+                j[1] = 255
+            if(j[2]>255):
+                j[2] = 255
+    result_image = Image.fromarray(np.uint8(normalized_img))
+    return result_image
