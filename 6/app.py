9fb5f8b003a3985869e28c3bae4c32d77b36c491
um <Fahrurrozi31@gmail.com>
2018-10-17 02:03:51 +0700

    finishing ui

--- 6/app.py
+++ 6/app.py
@@ -6,6 +6,7 @@ from chaincode import *
 from thinning import *
 import pickle
 import matplotlib
+import numpy as np
 matplotlib.use('Agg')
 
 from matplotlib import pyplot as plt
@@ -25,140 +26,17 @@ app_bone = []
 def main():
     return render_template('index.html')
 
-@app.route("/1")
+@app.route("/1", methods=['GET'])
 def main1():
     return render_template('1.html')
 
-@app.route("/2")
-def main2():
-    return render_template('2.html')
-
-@app.route("/3", methods=['GET'])
-def main3get():
-    return render_template('3.html', url_before = '', url_after = '')
-
-@app.route("/3", methods=['POST'])
-def main3():
-    if 'imgFile' not in request.files:
-        return render_template('3.html')
-    file = request.files['imgFile']
-    if file.filename == '':
-        return render_template('3.html')
-    image = request.files['imgFile']
-    img_path = 'static/images/image.png'
-    image.save(app.root_path + '/' + img_path)
-    equalizers = [0 for i in range(6)] 
-    for i in range(6):
-        equalizers[i] = int(request.form['myRange' + str(i+1)])
-    to_edit = Image.open(app.root_path + '/' + img_path)
-    new_image = equalize(to_edit, equalizers, app.root_path)
-    norm_img_path = 'static/images/normalized_image.png'
-    new_image.save(app.root_path + '/' + norm_img_path)
-        
-    return render_template('3.html', url_before = img_path + '?' + str(time.time()), url_after = norm_img_path + '?' + str(time.time()))
-
-@app.route("/4", methods=['GET'])
-def main4get():
-    return render_template('4.html', url_before = '', url_after = '')
-
-@app.route("/4", methods=['POST'])
-def main4post():
-    if 'imgFile' not in request.files:
-        return render_template('4.html')
-    file = request.files['imgFile']
-    if file.filename == '':
-        return render_template('4.html')
-    image = request.files['imgFile']
-    img_path = 'static/images/image.png'
-    image.save(app.root_path + '/' + img_path)
-    chain = build_chaincode(img_path, app.root_path)
-    with open(app.root_path + '/static/pickle/knn.pickle','rb') as handle:
-        knn = pickle.load(handle)
-    pred = knn.predict(chain)
-    return render_template('4.html', angka = pred[0])
-
-@app.route("/test", methods=['POST'])
-def main4posttest():
+@app.route("/1", methods=['POST'])
+def show_histogram():
     if 'imgFile' not in request.files:
         return json.dumps({'status':'Error1'})
     file = request.files['imgFile']
     if file.filename == '':
         return json.dumps({'status':'Error2'})
-    image = request.files['imgFile']
-    img_path = 'static/images/image.png'
-    image.save(app.root_path + '/' + img_path)
-    chain = build_chaincode(img_path, app.root_path)
-    with open(app.root_path + '/static/pickle/knn.pickle','rb') as handle:
-        knn = pickle.load(handle)
-    pred = knn.predict(chain)
-    return json.dumps({'angka': pred[0]})
-
-@app.route("/5", methods=['GET'])
-def main5get():
-    return render_template('5.html', url_before = '', url_after = '')
-
-@app.route("/5", methods=['POST'])
-def main5post():
-    if 'imgFile' not in request.files:
-        return render_template('5.html')
-    file = request.files['imgFile']
-    if file.filename == '':
-        return render_template('5.html')
-    image = request.files['imgFile']
-    img_path = 'static/images/image.png'
-    image.save(app.root_path + '/' + img_path)
-    bone = thinning(img_path, app.root_path)
-    bone_img = toimage(bone)
-    bone_img_path = 'static/images/bone_image.png' 
-    bone_img.save(app.root_path + '/' + bone_img_path)
-    return render_template('5.html', image_url = bone_img_path + '?' + str(time.time()))
-
-@app.route("/6", methods=['GET'])
-def main6get():
-    return render_template('6.html', url_before = '', url_after = '')
-
-@app.route("/6", methods=['POST'])
-def main6post():
-    if 'imgFile' not in request.files:
-        return render_template('6.html')
-    file = request.files['imgFile']
-    if file.filename == '':
-        return render_template('6.html')
-    image = request.files['imgFile']
-    img_path = 'static/images/image.png'
-    image.save(app.root_path + '/' + img_path)
-    bone = thinning(img_path, app.root_path)
-    bone_img = toimage(bone)
-    bone_img_path = 'static/images/bone_image.png' 
-    global app_bone
-    app_bone = bone
-    bone_img.save(app.root_path + '/' + bone_img_path)
-    return render_template('6.html', image_url = bone_img_path + '?' + str(time.time()))
-
-@app.route("/ascii", methods=['GET'])
-def asciiget():
-    return render_template('6.html', url_before = '', url_after = '')
-
-@app.route("/ascii", methods=['POST', 'GET'])
-def predict_ascii():
-    global app_bone
-    if(app_bone == []):
-        return render_template('6.html')
-    bone_img_path = 'static/images/bone_image.png'
-    array_feature = get_feature_from_bone(app_bone)
-    #Predict here
-    karakter = "a"
-    return render_template('6.html', image_url = bone_img_path + '?' + str(time.time()), karakter = karakter, fitur = array_feature)
-
-@app.route("/histogram", methods=['POST'])
-def show_histogram():
-    if 'imgFile' not in request.files:
-        return 'No selected file'
-    file = request.files['imgFile']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        return 'No selected file'
     image = request.files['imgFile']
     color = request.form.get('color')
     image.save(app.root_path + '/' + os.path.join(app.config['UPLOAD_FOLDER'], 'image.png'))
@@ -186,17 +64,19 @@ def show_histogram():
         plt.plot(build_hist(UPLOAD_FOLDER + '/image.png', color, app.root_path), color=plotcolor)
 
     plt.savefig(app.root_path + '/' + 'static/images/plot.png')
-    return render_template('plot.html', title = colorname + ' histogram', url ='static/images/plot.png?' + str(time.time()))
+    return json.dumps({'url_after': 'static/images/plot.png?' + str(time.time()) })
 
-@app.route("/normalize", methods=['POST'])
+@app.route("/2", methods=['GET'])
+def main2():
+    return render_template('2.html')
+
+@app.route("/2", methods=['POST'])
 def show_normalized():
     if 'imgFile' not in request.files:
-        return 'No selected file'
+        return json.dumps({'status':'Error1'})
     file = request.files['imgFile']
-    # if user does not select file, browser also
-    # submit an empty part without filename
     if file.filename == '':
-        return 'No selected file'
+        return json.dumps({'status':'Error2'})
     image = request.files['imgFile']
     method = request.form.get('method')
     img_path = 'static/images/image.png'
@@ -217,9 +97,88 @@ def show_normalized():
         norm_img_path = 'static/images/normalized_image.png'
         normalized_img.save(app.root_path + '/' + norm_img_path)
         
-    return render_template('result.html', title = 'Normalized Picture (' + title + ')', url_before = img_path + '?' + str(time.time()), url_after = norm_img_path + '?' + str(time.time()))
+    return json.dumps({'url_after': norm_img_path + '?' + str(time.time()) })
+
+@app.route("/3", methods=['GET'])
+def main3get():
+    return render_template('3.html', url_before = '', url_after = '')
+
+@app.route("/3", methods=['POST'])
+def main3():
+    if 'imgFile' not in request.files:
+        return json.dumps({'status':'Error1'})
+    file = request.files['imgFile']
+    if file.filename == '':
+        return json.dumps({'status':'Error2'})
+    image = request.files['imgFile']
+    img_path = 'static/images/image.png'
+    image.save(app.root_path + '/' + img_path)
+    equalizers = [0 for i in range(6)] 
+    for i in range(6):
+        equalizers[i] = int(request.form['myRange' + str(i+1)])
+    to_edit = Image.open(app.root_path + '/' + img_path)
+    new_image = equalize(to_edit, equalizers, app.root_path)
+    norm_img_path = 'static/images/normalized_image.png'
+    new_image.save(app.root_path + '/' + norm_img_path)
+        
+    return json.dumps({'url_after': norm_img_path + '?' + str(time.time()) })
+
+@app.route("/4", methods=['GET'])
+def main4get():
+    return render_template('4.html', url_before = '', url_after = '')
+
+@app.route("/4", methods=['POST'])
+def main4post():
+    if 'imgFile' not in request.files:
+        return json.dumps({'status':'Error1'})
+    file = request.files['imgFile']
+    if file.filename == '':
+        return json.dumps({'status':'Error2'})
+    image = request.files['imgFile']
+    img_path = 'static/images/image.png'
+    image.save(app.root_path + '/' + img_path)
+    chain = build_chaincode(img_path, app.root_path)
+    with open(app.root_path + '/static/pickle/knn.pickle','rb') as handle:
+        knn = pickle.load(handle)
+    pred = knn.predict(chain)
+    return json.dumps({'angka': pred[0]})
+
+@app.route("/5", methods=['GET'])
+def main5get():
+    return render_template('5.html', url_before = '', url_after = '')
+
+@app.route("/5", methods=['POST'])
+def main5post():
+    if 'imgFile' not in request.files:
+        return json.dumps({'status':'Error1'})
+    file = request.files['imgFile']
+    if file.filename == '':
+        return json.dumps({'status':'Error2'})
+    image = request.files['imgFile']
+    img_path = 'static/images/image.png'
+    image.save(app.root_path + '/' + img_path)
+    bone = thinning(img_path, app.root_path)
+    global app_bone
+    app_bone = bone
+    bone_img = toimage(bone)
+    bone_img_path = 'static/images/bone_image.png'
+    bone_img.save(app.root_path + '/' + bone_img_path)
+    return json.dumps({'url_after': bone_img_path + '?' + str(time.time()) })
+
+@app.route("/6", methods=['GET'])
+def main6get():
+    return render_template('6.html', url_before = '', url_after = '')
+
+@app.route("/6", methods=['POST'])
+def predict_ascii():
+    global app_bone
+    if (app_bone == []):
+        return json.dumps({'status':'Error1'})
+    array_feature = get_feature_from_bone(app_bone)
+    #Predict here
+    karakter = "a"
+    return json.dumps({'karakter': karakter })
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0',port=8111)
+    # app.run(host='0.0.0.0', port=3131, debug=True)
     app.run(host='0.0.0.0',port=os.environ['PORT'])
-
