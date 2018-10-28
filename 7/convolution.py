bbea1624e7cde0ffef091a468973bde96033d537
um <Fahrurrozi31@gmail.com>
2018-10-29 02:22:16 +0700

    add tugas 7

---
+++ 7/convolution.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import numpy as np
+
+def median(arr):
+  return int(np.median([x for x in arr if x != -1]))
+
+def gradient(arr):
+  return max([abs(arr[0+i]-arr[8-i]) for i in range(4)])
+
+def difference(arr):
+  return max([abs(arr[i]-arr[4]) for i in range(9)])
+
+def conv(image, root_path, method):
+  # open image
+  img = Image.open(root_path + '/' + image)
+  
+  # convert to grayscale
+  img = img.convert('L')
+  img_pix = img.load()
+
+  new_img = img.copy()
+  new_img_pix = new_img.load()
+
+  # convolution
+  width, height = img.size
+  for i in range(width):
+    for j in range(height):
+      arr = []
+      for w in [-1, 0, 1]:
+        for h in [-1, 0, 1]:
+          x = i + w
+          y = j + h
+          arr.append(img_pix[x, y]) if x >= 0 and y >= 0 and x < width and y < height else arr.append(-1)
+      new_img_pix[i, j] = median(arr) if method == '1' else gradient(arr) if method == '2' else difference(arr)
+  return new_img
---
+++ 7/rgbhist.py
@@ -0,0 +1,49 @@
+import PIL
+from PIL import Image
+import sys
+
+
+def build_hist(image, color, root_path):
+    im = Image.open(root_path + '/' + image)
+    loaded_img = im.load()
+    w, h = im.size
+    
+    if (color == 'a'):
+        r = [0] * 256    
+        g = [0] * 256    
+        b = [0] * 256    
+        x = [0] * 256
+    else :
+        histogram = [0] * 256
+
+    for i in range(w):
+        for j in range(h):
+            rgb_image = loaded_img[i,j]
+            if(color == 'a'):
+                r[rgb_image[0]] += 1
+                g[rgb_image[1]] += 1
+                b[rgb_image[2]] += 1
+                x[int((rgb_image[0] + rgb_image[1] + rgb_image[2]) / 3)] += 1
+            elif(color == 'r'):
+                histogram[rgb_image[0]] += 1
+            elif(color == 'g'):
+                histogram[rgb_image[1]] += 1
+            elif(color == 'b'):
+                histogram[rgb_image[2]] += 1
+            else:
+                histogram[int((rgb_image[0] + rgb_image[1] + rgb_image[2]) / 3)] += 1  
+
+    dim = float(w * h)
+    if (color == 'a'):
+        hist = [[0] for i in range(4)]
+        for i in range(0, 256):
+            hist[0].append(float(r[i])/dim)
+            hist[1].append(float(g[i])/dim)
+            hist[2].append(float(b[i])/dim)
+            hist[3].append(float(x[i])/dim)
+    else :
+        hist = []
+        for k in histogram:
+            hist.append(float(k)/dim)
+    return hist
+    
